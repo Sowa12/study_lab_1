6fe5d26d70226d4c546dd5b2851b2e2564356816
vip.kuzneccov@mail.ru <vip.kuzneccov@mail.ru>
2025-03-02 15:06:06 +0500

    Добавление conanfile.txt

---
+++ tests/test_version_gtest.cpp
@@ -0,0 +1,16 @@
+#define BOOST_TEST_MODULE test_version
+
+#include <lib.h>
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+TEST(VersionTest, Positive) {
+	EXPECT_TRUE(version() > 0);
+}
+
+int main() {
+
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
